1206c601da06072b1caeb96d73bd12f8fe27d403
384606266@qq.com <maicha@maicha-14pro.localdomain>
2023-10-06 22:32:39 +0800

    modify research page

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@
 const accordionPapers = document.getElementById('accordionPanelsStayOpenPapers')
 const accordionProgress = document.getElementById('accordionPanelsStayOpenProgress')
 const listPolicy = document.getElementById('listPolicy')
+const accordionExperience = document.getElementById('accordionPanelsStayOpenExperience')
 
 async function papers(){
     const response = await fetch ('../papers.json')
@@ -61,4 +62,27 @@ policy().then(data =>{
         item.setAttribute("class", "list-item");
         listPolicy.appendChild(item)
     })
+})
+
+async function experience(){
+    const response = await fetch ('../experience.json')
+    return await response.json()
+}
+
+experience().then(data =>{
+    data.forEach(element => {
+        let item = document.createElement("div");
+        item.innerHTML = `<h2 class="accordion-header" id="panelsStayOpen-heading${element.id}">
+        <button class = "accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapse${element.id}" aria-expanded="true" aria-controls="panelsStayOpen-collapse${element.id}">${element.title}</button></h2>`;
+        item.setAttribute("class", "accordion-item");
+        accordionExperience.appendChild(item);
+
+        let body = document.createElement("div");
+        body.innerHTML = `<div class="accordion-body accordion-text"><i>${element.field}</i>\n<i>${element.time}</i>
+        <p>${element.text}</p></div>`;
+        body.setAttribute("id", `panelsStayOpen-collapse${element.id}`);
+        body.setAttribute("class", "accordion-collapse collapse");
+        body.setAttribute("aria-labelledby", "panelsStayOpen-headingOn");
+        item.appendChild(body);
+    });
 })
